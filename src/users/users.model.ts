44ee7da00447cb8ddb9335f6bf374257936606fa
Maxim Teslenko <charlie.groupdev@gmail.com>
2022-01-10 23:31:30 +0200

    init project

---
+++ src/users/users.model.ts
@@ -0,0 +1,46 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+/*Mandatory fields to create user object*/
+interface UserCreationAttrs {
+  name: string;
+  email: string;
+  password: string;
+  icon: string;
+}
+
+@Table({ tableName: "users" })
+export class User extends Model<User, UserCreationAttrs> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  name: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+    defaultValue: "",
+  })
+  icon: string;
+}
